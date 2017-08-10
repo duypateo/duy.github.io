cd5ef702b6a41a1046a30a7a338c70272a2f3601
Nguyen Tuong Duy <duy.pateo@gmail.com>
2017-08-10 15:13:29 +0700

    Responsive

--- js/app.js
+++ js/app.js
@@ -3,42 +3,39 @@ jQuery(document).ready(function($) {
 
 	var menuLink = $('.main-nav ul li a');
 	var page = $('.page');
-	var menuBtn = $('.menu-button');
-	var mobileNav = $('.mobile-menu');
-	var mNavCancel = $('.close-wrap img');
-
+	var menuBtn = $('.mobile-btn');
+	var mobileNav = $('.core-menu');
+	var mNavCancel = $('.active');
 	page.css('visibility', 'hidden');
-
+	// TOGGLE PAGE
 	$('#home').addClass('slideInUp').css('visibility', 'visible');
 	menuLink.on('click', function(event) {
 		event.preventDefault();
 		var linkRef = $(this).attr('href');
-
 		menuLink.parent('li').removeClass('active-line');
 		$(this).parent('li').addClass('active-line');
-
 		page.removeClass('slideInUp').addClass('slideOutDown');
 		$(linkRef).removeClass('slideOutDown').addClass('slideInUp').css('visibility', 'visible');
 	});
-
-
+	// TOGGLE MOBILE MENU
 	menuBtn.on('click', function(event) {
 		event.preventDefault();
-		mobileNav.removeClass('slideLeftOut').addClass('slideLeftIn').css('visibility', 'visible');
-
+		if(!$(this).hasClass('active')){
+			mobileNav.removeClass('slideLeftOut').addClass('slideLeftIn').css('visibility', 'visible');
+			$(this).addClass('active');
+		}else{
+			mobileNav.removeClass('slideLeftIn').addClass('slideLeftOut');
+			$(this).removeClass('active');
+		}
 	});
-	mNavCancel.on('click', function(event) {
-		event.preventDefault();
-		mobileNav.removeClass('slideLeftIn').addClass('slideLeftOut');
-	});		mobileItem = $('.mobile-menu li a ');
-	mobileItem.on('click', function(event) {
-		event.preventDefault();
-		mobileNav.removeClass('slideLeftIn').addClass('slideLeftOut');
-	});
-
-	mobileItem = $('.mobile-menu li a ');
-	mobileItem.on('click', function(event) {
-		event.preventDefault();
-		mobileNav.removeClass('slideLeftIn').addClass('slideLeftOut');
+	// WINDOW RESIZE
+	$(window).resize(function(event) {
+		if(mobileNav.css('visibility')=='hidden'){
+			console.log('ok');
+			mobileNav.css('visibility','visible');
+		}
+		if(mobileNav.hasClass('slideLeftOut')){
+			mobileNav.removeClass('slideLeftOut');
+		}
 	});
 });
